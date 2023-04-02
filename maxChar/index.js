bc57574b8675889985ca44d9e263e04435bee792
lucasgojeda <lukitas.ojeda.00@gmail.com>
2023-04-02 19:00:38 -0300

    (5/28): maxChar completed

--- maxChar/index.js
+++ maxChar/index.js
@@ -3,7 +3,45 @@
 // maxChar("I loveeeeeee noodles") === "e"
 // maxChar("1337") === "3"
 
-function maxChar(str) {}
+/** Version 1 of the solution */
+const maxChar_1 = (str) => {
+  let objTable = {};
+  let res = null;
+  for (let i = 0; i < str.length; i++) {
+    if (!objTable[str[i]]) {
+      objTable[str[i]] = 1;
+      if (!res) res = { value: objTable[str[i]], name: str[i] };
+    } else {
+      objTable[str[i]] += 1;
+
+      if (res.value < objTable[str[i]]) {
+        res = { value: objTable[str[i]], name: str[i] };
+      }
+    }
+  }
+  return res.name;
+};
+
+/** Version 2 of the solution */
+const maxChar_2 = (str) => {
+  let charCountObj = {};
+  let maxChar = "";
+  let maxCount = "";
+
+  /** Fill out character count object */
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    charCountObj[char] = charCountObj[char] + 1 || 1;
+
+    if (charCountObj[char] > maxCount) {
+      maxChar = char;
+      maxCount = charCountObj[char];
+    }
+  }
+
+  return maxChar;
+};
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -33,10 +71,10 @@ const { assert } = chai;
 
 describe("Max Character", () => {
   it("maxChar() finds the most frequently used character", () => {
-    assert.equal(maxChar("a"), "a");
-    assert.equal(maxChar("test"), "t");
-    assert.equal(maxChar("I loveeeeee noodles"), "e");
-    assert.equal(maxChar("1337"), "3");
+    assert.equal(maxChar_2("a"), "a");
+    assert.equal(maxChar_2("test"), "t");
+    assert.equal(maxChar_2("I loveeeeee noodles"), "e");
+    assert.equal(maxChar_2("1337"), "3");
   });
 });
 
