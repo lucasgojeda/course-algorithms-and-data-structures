db616aa73f5e507fda2cbf4cee806531625ed1e5
lucasgojeda <lukitas.ojeda.00@gmail.com>
2023-04-03 16:17:17 -0300

    (9/28): anagrams completed

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,7 +8,101 @@
 //   anagrams('Heart!', 'EARTH') --> True
 //   anagrams('lol', 'lolc') --> False
 
-function anagrams(stringA, stringB) {}
+/** Version 1 of the solution */
+const anagrams_1 = (stringA, stringB) => {
+  const cleanstringA = stringA
+    .trim()
+    .replace(/([!])|(\s+)/g, "")
+    .toLowerCase();
+  const cleanstringB = stringB
+    .trim()
+    .replace(/([!])|(\s+)/g, "")
+    .toLowerCase();
+
+  if (cleanstringA.length !== cleanstringB.length) return false;
+
+  for (let i = 0; i < cleanstringB.length; i++) {
+    if (!cleanstringA.includes(cleanstringB[i])) return false;
+  }
+
+  return true;
+};
+
+/** Version 2 of the solution */
+const anagrams_2 = (stringA, stringB) => {
+  const cleanstringA = stringA
+    .trim()
+    .replace(/([!])|(\s+)/g, "")
+    .toLowerCase();
+  const cleanstringB = stringB
+    .trim()
+    .replace(/([!])|(\s+)/g, "")
+    .toLowerCase();
+
+  if (cleanstringA.length !== cleanstringB.length) return false;
+
+  let charCountObjA = {};
+  let charCountObjB = {};
+
+  for (let i = 0; i < cleanstringA.length; i++) {
+    const charA = cleanstringA[i];
+    const charB = cleanstringB[i];
+
+    charCountObjA[charA] = charCountObjA[charA] + 1 || 1;
+    charCountObjB[charB] = charCountObjB[charB] + 1 || 1;
+  }
+
+  for (let i = 0; i < cleanstringA.length; i++) {
+    const charA = cleanstringA[i];
+    if (charCountObjA[charA] !== 1 || charCountObjB[charA] !== 1) return false;
+  }
+
+  return true;
+};
+
+/** Version 3 of the solution */
+const anagrams_3 = (stringA, stringB) => {
+  stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
+  stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
+
+  if (stringA.length !== stringB.length) return false;
+
+  const stringACharCount = {};
+
+  for (let i = 0; i < stringA.length; i++) {
+    const aChar = stringA[i];
+    stringACharCount[aChar] = stringACharCount[aChar] + 1 || 1;
+  }
+
+  for (let i = 0; i < stringB.length; i++) {
+    const bChar = stringB[i];
+
+    if (!stringACharCount[bChar]) {
+      return false;
+    } else {
+      stringACharCount[bChar]--;
+    }
+  }
+
+  return true;
+};
+
+/** Version 3 of the solution */
+const anagrams_4 = (stringA, stringB) => {
+  stringA = stringA
+    .toLowerCase()
+    .replace(/[\W_]+/g, "")
+    .split("")
+    .sort()
+    .join("");
+  stringB = stringB
+    .toLowerCase()
+    .replace(/[\W_]+/g, "")
+    .split("")
+    .sort()
+    .join("");
+  return stringA === stringB;
+};
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -38,23 +132,23 @@ const { assert } = chai;
 
 describe("Anagrams", () => {
   it("works if case sensitivity and non word characters NOT taken into account", () => {
-    assert.equal(anagrams("earth", "heart"), true);
+    assert.equal(anagrams_4("earth", "heart"), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
+    assert.equal(anagrams_4("love", "meow"), false);
+    assert.equal(anagrams_4("lol", "lolc"), false);
   });
   it("is case insensitive. 'HEART' and 'earth' should return true", () => {
-    assert.equal(anagrams("HEART", "earth"), true);
-    assert.equal(anagrams("heart", "EARTH"), true);
+    assert.equal(anagrams_4("HEART", "earth"), true);
+    assert.equal(anagrams_4("heart", "EARTH"), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
+    assert.equal(anagrams_4("love", "meow"), false);
+    assert.equal(anagrams_4("lol", "lolc"), false);
   });
   it("only matches word characters. 'heart!'' and '' earth' should return true", () => {
-    assert.equal(anagrams("heart!", " earth"), true);
+    assert.equal(anagrams_4("heart!", " earth"), true);
 
-    assert.equal(anagrams("love", "meow"), false);
-    assert.equal(anagrams("lol", "lolc"), false);
+    assert.equal(anagrams_4("love", "meow"), false);
+    assert.equal(anagrams_4("lol", "lolc"), false);
   });
 });
 
