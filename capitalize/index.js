a274179493d3486a84dfed30c381b69e34b13d65
lucasgojeda <lukitas.ojeda.00@gmail.com>
2023-04-03 10:58:52 -0300

    (7/28): capitalize completed

--- capitalize/index.js
+++ capitalize/index.js
@@ -5,7 +5,35 @@
 //   capitalize("I'm a little tea pot") --> 'I'm A Little Tea Pot'
 //   capitalize('sHoRt AnD sToUt') --> 'Short And Stout'
 
-function capitalize(str) {}
+/** Version 1 of the solution */
+const capitalize_1 = (str) => {
+  const strArr = str.split(" ");
+  let res = "";
+
+  for (let i = 0; i < strArr.length; i++) {
+    const newStr =
+      strArr[i].substring(0, 1).toUpperCase() +
+      strArr[i].substring(1, strArr[i].length).toLowerCase();
+    res = res ? res + " " + newStr : newStr;
+  }
+
+  console.log(res);
+  return res;
+};
+
+/** Version 2 of the solution */
+const capitalize_2 = (str) => {
+  const arrOfWords = str.split(" ");
+  const arrOfWordsCased = [];
+
+  for (let i = 0; i < arrOfWords.length; i++) {
+    const word = arrOfWords[i];
+    arrOfWordsCased.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+  }
+
+  console.log(arrOfWordsCased.join(" "));
+  return arrOfWordsCased.join(" ");
+};
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -35,10 +63,10 @@ const { assert } = chai;
 
 describe("Capitalization", () => {
   it("capitalizes the first letter of each word", () => {
-    assert.equal(capitalize("I'm a little tea pot"), "I'm A Little Tea Pot");
+    assert.equal(capitalize_2("I'm a little tea pot"), "I'm A Little Tea Pot");
   });
   it("lowercases the rest of the word", () => {
-    assert.equal(capitalize("sHoRt AnD sToUt"), "Short And Stout");
+    assert.equal(capitalize_2("sHoRt AnD sToUt"), "Short And Stout");
   });
 });
 
