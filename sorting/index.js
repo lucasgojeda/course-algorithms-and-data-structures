04f0f69e72841f3d926d709f9dc9acda5e28a600
lucasgojeda <lukitas.ojeda.00@gmail.com>
2023-04-06 12:40:15 -0300

    (17/28): sorting completed

--- sorting/index.js
+++ sorting/index.js
@@ -1,11 +1,34 @@
 // Implement a swap helper function that we will use in both BS and SS
-function swap(arr, i, j) {}
+/** Time complexity O(1) */
+const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
 
 //bubbleSort works by having sorted data accumulate at end of array
-function bubbleSort(arr) {}
+/** Time complexity O(n^2) */
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+};
 
 //selectionSort works by having sorted data accumulate at start of array
-function selectionSort(arr) {}
+/** Time complexity O(n^2) */
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let swapIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[swapIdx]) {
+        swapIdx = j;
+      }
+    }
+    swap(arr, i, swapIdx);
+  }
+  return arr;
+};
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -41,13 +64,13 @@ describe("swap()", () => {
   });
 });
 
-describe.skip("Bubble Sort", () => {
+describe("Bubble Sort", () => {
   it("sorts an array", () => {
     assert.deepEqual(bubbleSort([5, 1, 3, 7, 6, 2, 4]), [1, 2, 3, 4, 5, 6, 7]);
   });
 });
 
-describe.skip("Selection Sort", () => {
+describe("Selection Sort", () => {
   it("sorts an array", () => {
     assert.deepEqual(selectionSort([5, 1, 3, 2, 4]), [1, 2, 3, 4, 5]);
   });
